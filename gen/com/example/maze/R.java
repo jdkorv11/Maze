b3cd32ebf3dcf86947e7335b660456b69f0501e4
jdkorv11 <jdkorv11@smumn.edu>
2014-04-30 13:01:17 -0500

    added google apis

--- gen/com/example/maze/R.java
+++ gen/com/example/maze/R.java
@@ -10,10 +10,8 @@ package com.example.maze;
 public final class R {
     public static final class array {
         public static final int level_complete_options=0x7f050000;
-        public static final int pref_visual_theme_list =0x7f050001;
-        public static final int pref_visual_theme_list_values =0x7f050002;
-        public static final int pref_sync_frequency_titles=0x7f050003;
-        public static final int pref_sync_frequency_values=0x7f050004;
+        public static final int pref_visual_theme_list=0x7f050001;
+        public static final int pref_visual_theme_list_values=0x7f050002;
     }
     public static final class attr {
     }
@@ -28,50 +26,60 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int btn_down =0x7f020000;
-        public static final int end_screen=0x7f020001;
-        public static final int industrial_finish =0x7f020002;
-        public static final int industrial_guy_east =0x7f020003;
-        public static final int industrial_guy_north =0x7f020004;
-        public static final int industrial_guy_south =0x7f020005;
-        public static final int industrial_guy_west =0x7f020006;
-        public static final int btn_left =0x7f020007;
-        public static final int industrial_path =0x7f020008;
-        public static final int industrial_preview =0x7f020009;
-        public static final int btn_right =0x7f02000a;
-        public static final int industrial_start =0x7f02000b;
-        public static final int btn_up =0x7f02000c;
-        public static final int industrial_wall =0x7f02000d;
+        public static final int btn_down=0x7f020000;
+        public static final int btn_left=0x7f020001;
+        public static final int btn_right=0x7f020002;
+        public static final int btn_up=0x7f020003;
+        public static final int high_score=0x7f020004;
+        public static final int industrial_finish=0x7f020005;
+        public static final int industrial_guy_east=0x7f020006;
+        public static final int industrial_guy_north=0x7f020007;
+        public static final int industrial_guy_south=0x7f020008;
+        public static final int industrial_guy_west=0x7f020009;
+        public static final int industrial_path=0x7f02000a;
+        public static final int industrial_preview=0x7f02000b;
+        public static final int industrial_start=0x7f02000c;
+        public static final int industrial_wall=0x7f02000d;
+        public static final int retro_finish=0x7f02000e;
+        public static final int retro_guy_east=0x7f02000f;
+        public static final int retro_guy_north=0x7f020010;
+        public static final int retro_guy_south=0x7f020011;
+        public static final int retro_guy_west=0x7f020012;
+        public static final int retro_path=0x7f020013;
+        public static final int retro_preview=0x7f020014;
+        public static final int retro_wall=0x7f020015;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0016;
-        public static final int button1=0x7f0a0000;
-        public static final int downBtn=0x7f0a000f;
-        public static final int gridView=0x7f0a0008;
-        public static final int highScoresList=0x7f0a0005;
-        public static final int hsLevelSelect=0x7f0a0007;
-        public static final int hsMainMenu=0x7f0a0006;
-        public static final int hslist_item_name=0x7f0a0003;
-        public static final int hslist_item_score=0x7f0a0004;
-        public static final int leftBtn=0x7f0a0011;
-        public static final int level_select_item_level_number=0x7f0a0009;
-        public static final int main_high_scores=0x7f0a000a;
-        public static final int main_options=0x7f0a000c;
-        public static final int main_start_game=0x7f0a000b;
-        public static final int mazeHolder=0x7f0a000e;
-        public static final int maze_activity_directions =0x7f0a000d;
-        public static final int rightBtn=0x7f0a0010;
-        public static final int submit_dialog_player=0x7f0a0014;
-        public static final int submit_dialog_score=0x7f0a0013;
-        public static final int submit_score_button=0x7f0a0015;
-        public static final int textView1=0x7f0a0001;
-        public static final int textView2=0x7f0a0002;
-        public static final int upBtn=0x7f0a0012;
+        public static final int action_settings=0x7f0a0010;
+        public static final int gridView=0x7f0a0005;
+        public static final int highScoresList=0x7f0a0002;
+        public static final int hsLevelSelect=0x7f0a0004;
+        public static final int hsMainMenu=0x7f0a0003;
+        public static final int hslist_item_name=0x7f0a0000;
+        public static final int hslist_item_score=0x7f0a0001;
+        public static final int level_select_item_level_number=0x7f0a0007;
+        public static final int level_select_preview=0x7f0a0006;
+        public static final int main_high_scores=0x7f0a0008;
+        public static final int main_options=0x7f0a000a;
+        public static final int main_start_game=0x7f0a0009;
+        public static final int mazeHolder=0x7f0a000c;
+        public static final int maze_activity_directions=0x7f0a000b;
+        public static final int submit_dialog_player=0x7f0a000e;
+        public static final int submit_dialog_score=0x7f0a000d;
+        public static final int submit_score_button=0x7f0a000f;
     }
     public static final class layout {
-        public static final int congratulations_display=0x7f030000;
-        public static final int high_score_list_item=0x7f030001;
-        public static final int high_scores=0x7f030002;
+        public static final int high_score_list_item=0x7f030000;
+        /** 
+    Layout alias to replace the single-pane version of the layout with a
+    two-pane version on Large screens.
+
+    For more on layout aliases, see:
+    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
+    
+         */
+        public static final int high_scores=0x7f030001;
+        public static final int high_scores_large=0x7f030002;
         public static final int level_select=0x7f030003;
         public static final int level_select_list_item=0x7f030004;
         public static final int main_activity=0x7f030005;
@@ -84,42 +92,31 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
-        public static final int congratulations=0x7f070006;
-        public static final int high_scores=0x7f070008;
-        public static final int instructioins=0x7f070005;
-        public static final int level_select=0x7f070007;
-        public static final int level_select_instructions=0x7f07000b;
-        public static final int main_menu=0x7f07000e;
-        public static final int options=0x7f07000a;
-        public static final int options_region=0x7f07000f;
-        public static final int options_theme=0x7f070010;
+        public static final int congratulations=0x7f070005;
+        public static final int high_scores=0x7f070007;
+        public static final int level_select=0x7f070006;
+        public static final int level_select_instructions=0x7f07000a;
+        public static final int main_menu=0x7f07000d;
+        public static final int maze_directions=0x7f07000f;
+        public static final int options=0x7f070009;
+        public static final int options_theme=0x7f07000e;
         public static final int playAgainBtn=0x7f070003;
-        public static final int pref_default_display_name=0x7f070016;
-        public static final int pref_description_social_recommendations=0x7f070014;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_sync_data =0x7f070018;
+        public static final int pref_header_sync_data=0x7f070013;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_theme =0x7f070012;
-        /**  Example settings for Notifications 
-         */
-        public static final int pref_header_region =0x7f07001b;
-        public static final int pref_ringtone_silent=0x7f07001e;
-        public static final int pref_title_choose_visual_theme =0x7f070017;
-        public static final int pref_title_display_name=0x7f070015;
-        public static final int pref_title_new_message_notifications=0x7f07001c;
-        public static final int pref_title_ringtone=0x7f07001d;
-        public static final int pref_title_social_recommendations=0x7f070013;
-        public static final int pref_title_sync_frequency=0x7f070019;
-        public static final int pref_title_system_sync_settings=0x7f07001a;
-        public static final int pref_title_vibrate=0x7f07001f;
+        public static final int pref_header_theme=0x7f070011;
+        public static final int pref_summary_sync_data=0x7f070016;
+        public static final int pref_switch_off=0x7f070015;
+        public static final int pref_switch_on=0x7f070014;
+        public static final int pref_title_choose_visual_theme=0x7f070012;
         public static final int solve=0x7f070002;
-        public static final int start_game=0x7f070009;
-        public static final int submit=0x7f07000c;
-        public static final int submit_dialog_score_pt1=0x7f07000d;
-        public static final int title_activity_options=0x7f070011;
+        public static final int start_game=0x7f070008;
+        public static final int submit=0x7f07000b;
+        public static final int submit_dialog_score_pt1=0x7f07000c;
+        public static final int title_activity_options=0x7f070010;
         public static final int youWin=0x7f070004;
     }
     public static final class style {
@@ -152,8 +149,7 @@ public final class R {
     }
     public static final class xml {
         public static final int pref_data_sync=0x7f040000;
-        public static final int pref_themes =0x7f040001;
-        public static final int pref_headers=0x7f040002;
-        public static final int pref_region =0x7f040003;
+        public static final int pref_headers=0x7f040001;
+        public static final int pref_themes=0x7f040002;
     }
 }
