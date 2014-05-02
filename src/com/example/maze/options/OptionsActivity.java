e801be71e9f328e139b3918fb3f7788a79eca093
jdkorv11 <jdkorv11@smumn.edu>
2014-05-02 09:25:00 -0500

    fixed simple options error

--- src/com/example/maze/options/OptionsActivity.java
+++ src/com/example/maze/options/OptionsActivity.java
@@ -89,10 +89,8 @@ public class OptionsActivity extends PreferenceActivity {
         // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
         // their values. When their values change, their summaries are updated
         // to reflect the new value, per the Android Design guidelines.
-        bindPreferenceSummaryToValue(findPreference("example_text"));
-        bindPreferenceSummaryToValue(findPreference("example_list"));
-        bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
-        bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        bindPreferenceSummaryToValue(findPreference("pref_theme_list"));
+        bindPreferenceSummaryToValue(findPreference("pref_sync_switch"));
     }
 
     /**
